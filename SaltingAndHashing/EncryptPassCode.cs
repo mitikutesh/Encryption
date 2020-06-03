d69527241e73401512098cf077030c2b9995c5c9
Mitiku Geleta <mitikutesh@gmail.com>
2020-06-03 23:50:05 +0300

    simple options

---
+++ SaltingAndHashing/EncryptPassCode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SaltingAndHashing
+{
+    public class EncryptPassCode
+    {
+        public void GenerateSaltSHA256()
+        {
+            var salt = CreateSalt(10);
+            var hashpassword = GenerateSHA256Hash("test", salt);
+            Console.WriteLine(hashpassword);
+        }
+
+        public string CreateSalt(int size)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buff = new byte[size];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public string GenerateSHA256Hash(string input, string salt)
+        {
+            
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            SHA256Managed sha256String = new SHA256Managed();
+            byte[] hash = sha256String.ComputeHash(bytes);
+            return ByteArrayToHexString(hash);
+        }
+        static byte[] GenerateSaltedHash(string input, int size)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            byte[] salt = new byte[size];
+            rng.GetBytes(salt);
+            
+            
+            byte[] plainText = Encoding.UTF8.GetBytes(input);
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            byte[] plainTextWithSaltBytes = 
+                new byte[plainText.Length + salt.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);            
+        }
+        public string ByteArrayToHexString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (var b in ba)
+            {
+                hex.AppendFormat("{0:x2}", b);
+                
+            }
+            return hex.ToString();
+        }
+
+        public byte[] HextStringTOByteArray(string hex)
+        {
+            int numberChars = hex.Length;
+            byte[]bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i++)
+            {
+                bytes[1 / 2] = Convert.ToByte(hex.Substring(1, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
--- SaltingAndHashing/Program.cs
+++ SaltingAndHashing/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace SaltingAndHashing
 {
     class Program
     {
+        private const int SaltSize = 32;
+        private const int HashSize = 32;
+        private const int IterationCount = 10000;
+        
+        
         static void Main(string[] args)
         {
+            string password = "MySecret";
+            // EncryptPassCode enc = new EncryptPassCode();
+            // enc.GenerateSaltSHA256();
+            string passwordHash = GeneratePasswordHash(password, out string salt);
+           var k =  VerifyPassword(password, passwordHash, salt);
+           var c =  VerifyPassword(password.ToUpper(), passwordHash, salt);
             Console.WriteLine("Hello World!");
         }
+        public static string GeneratePasswordHash(string password, out string salt)
+        {
+            using (Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltSize))
+            {
+                rfc2898DeriveBytes.IterationCount = IterationCount;
+                byte[] hashData = rfc2898DeriveBytes.GetBytes(HashSize);
+                byte[] saltData = rfc2898DeriveBytes.Salt;
+                salt = Convert.ToBase64String(saltData);
+                return Convert.ToBase64String(hashData);
+            }
+        }
+        public static bool VerifyPassword(string password, string passwordHash, string salt)
+        {
+            
+            using (Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltSize))
+            {
+                rfc2898DeriveBytes.IterationCount = IterationCount;
+                rfc2898DeriveBytes.Salt = Convert.FromBase64String(salt);
+                byte[] hashData = rfc2898DeriveBytes.GetBytes(HashSize);
+                return Convert.ToBase64String(hashData) == passwordHash;
+            }
+        }
+        
     }
 }
